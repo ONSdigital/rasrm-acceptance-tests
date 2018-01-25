4595c4b11ae2c1101cac943c8ac9f2de768477bd
andrewmil <Andrew.Millar@qa.com>
2018-01-25 11:50:40 +0000

    Us005 view events (#7)
    Add scenario to check event dates on collection exercise details
    Update tests to use new format of survey info in collection exercise details

--- acceptance_tests/features/pages/collection_exercise_details.py
+++ acceptance_tests/features/pages/collection_exercise_details.py
@@ -15,10 +15,22 @@ def get_page_title():
 def get_collection_exercise_details():
     ce_details = {
         "survey_info": browser.find_by_name('survey-info').value,
+        "survey_title": browser.find_by_name('survey-title').value,
         "period": browser.find_by_name('period').value,
         "user_description": browser.find_by_name('user-description').value
     }
     return ce_details
+
+
+def get_collection_exercise_events():
+    ce_events = {
+        "mps": browser.find_by_name('mps-date').value,
+        "go_live": browser.find_by_name('go-live-date').value,
+        "return_by": browser.find_by_name('return-by-date').value,
+        "first_reminder": browser.find_by_name('first-reminder-date').value,
+        "exercise_end": browser.find_by_name('exercise-end-date').value
+    }
+    return ce_events
 
 
 def load_collection_instrument(test_file):
