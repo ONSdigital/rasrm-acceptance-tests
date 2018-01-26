16670917aa2eda9747d8e506daa65afffa8c935f
Andrew Millar <andrew.millar@qa.com>
2018-01-26 15:03:58 +0000

    Add tests for collection instrument validation

--- acceptance_tests/features/steps/load_seft_collection_instruments.py
+++ acceptance_tests/features/steps/load_seft_collection_instruments.py
@@ -13,9 +13,31 @@ def internal_user_views_collection_exercise(_):
     collection_exercise_details.go_to('QIFDI', '201803')
 
 
+@when('the internal user selects a non xlsx file')
+def internal_user_selects_non_xlsx_file(_):
+    collection_exercise_details.select_wrong_file_type('resources/collection_instrument_files/wrong_ci_type.html')
+
+
+@when('the internal user loads a non xlsx file')
+def internal_user_uploads_non_xlsx_file(_):
+    collection_exercise_details.load_collection_instrument('resources/collection_instrument_files/wrong_ci_type.html')
+
+
 @then('the user is able to load the collection instruments')
 def load_collection_instruments(_):
     collection_exercise_details.load_collection_instrument(
         test_file='resources/collection_instrument_files/064_0001_201803.xlsx')
     success_text = collection_exercise_details.get_collection_instrument_success_text()
     assert success_text == 'Collection instrument loaded'
+
+
+@then('an error message is displayed to the user')
+def incorrect_file_type_message_shown(_):
+    error_text = collection_exercise_details.get_collection_instrument_error_text()
+    assert error_text == "Incorrect file type. Please choose a file type XLSX"
+
+
+@then('an error message is displayed to the user at the top of the screen')
+def incorrect_file_type_message_shown_at_top(_):
+    error_text = collection_exercise_details.get_error_header()
+    assert error_text == "Error: wrong file type for Collection instrument"
