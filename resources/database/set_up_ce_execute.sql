48fd753094173ffcd463518a8b774dd746e75d8d
Tejas Patel <tejas.patel@ons.gov.uk>
2018-02-01 11:59:40 +0000

    edit set up ce execution script

--- resources/database/set_up_ce_execute.sql
+++ resources/database/set_up_ce_execute.sql
@@ -1,28 +1,29 @@
--- Add the classifier for Bricks (survey_id cb8accda-6118-4d3b-85a3-149e28960c54)
 insert into survey.classifierType( classifiertypepk,classifiertypeselectorfk,classifiertype)
 values(4,17,'COLLECTION_EXERCISE');
--- Add the classifier for Blocks (survey_id 9b6872eb-28ee-4c09-b705-c3ab1bb0f9ec)
+
 insert into survey.classifierType( classifiertypepk,classifiertypeselectorfk,classifiertype)
 values(5,15,'COLLECTION_EXERCISE');
--- Add the action plans for Bricks cb8accda (074) and Blocks 9b6872eb (073)
+
 insert into action.actionplan(id, actionplanpk, name, description, createdby, lastrundatetime)
 values('878c3462-766a-4cf6-8fb3-867d5124cc0f',3,'Bricks and Blocks Enrolment','Bricks and Blocks Enrolment','SYSTEM',now());
+
 insert into action.actionplan(id, actionplanpk, name, description, createdby, lastrundatetime)
 values('46a4cdc2-8995-4969-a02d-37f6be767ef4',4,'Bricks and Blocks','Bricks and Blocks','SYSTEM',now());
--- Add actionplan links for Bricks
+
 insert into collectionexercise.casetypedefault(casetypedefaultpk, sampleunittypefk, actionplanid, survey_uuid)
 values(3,'B','878c3462-766a-4cf6-8fb3-867d5124cc0f','cb8accda-6118-4d3b-85a3-149e28960c54');
+
 insert into collectionexercise.casetypedefault(casetypedefaultpk, sampleunittypefk, actionplanid, survey_uuid)
 values(4,'BI','46a4cdc2-8995-4969-a02d-37f6be767ef4','cb8accda-6118-4d3b-85a3-149e28960c54');
--- Add actionplan links for Blocks
+
 insert into collectionexercise.casetypedefault(casetypedefaultpk, sampleunittypefk, actionplanid, survey_uuid)
 values(5,'B','878c3462-766a-4cf6-8fb3-867d5124cc0f','9b6872eb-28ee-4c09-b705-c3ab1bb0f9ec');
+
 insert into collectionexercise.casetypedefault(casetypedefaultpk, sampleunittypefk, actionplanid, survey_uuid)
 values(6,'BI','46a4cdc2-8995-4969-a02d-37f6be767ef4','9b6872eb-28ee-4c09-b705-c3ab1bb0f9ec');
 
--- Set the CE dates for 074 Bricks period 201801
 update collectionexercise.collectionexercise
-set scheduledstartdatetime = '2017-09-11 23:00:00+00',
+ set scheduledstartdatetime = '2017-09-11 23:00:00+00',
     scheduledexecutiondatetime ='2017-09-10 23:00:00+00',
     scheduledreturndatetime = '2018-02-28 00:00:00+00',
     scheduledenddatetime = '2018-06-29 23:00:00+00',
