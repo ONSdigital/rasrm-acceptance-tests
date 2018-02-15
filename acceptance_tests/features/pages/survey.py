cd2f7dbdd631a474de02ed8fb7616dd049035aad
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-15 09:13:30 +0000

    Use fstrings (#39)
    * Use fstrings

--- acceptance_tests/features/pages/survey.py
+++ acceptance_tests/features/pages/survey.py
@@ -3,7 +3,7 @@ from config import Config
 
 
 def go_to():
-    browser.visit("{}/surveys".format(Config.RESPONSE_OPERATIONS_UI))
+    browser.visit(f"{Config.RESPONSE_OPERATIONS_UI}/surveys")
 
 
 def get_page_title():
