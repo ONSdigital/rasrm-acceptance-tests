a83a55894cb0cf4311b62874e7718abcbba74bba
James Gardiner <james@jgardiner.co.uk>
2018-03-29 15:10:38 +0100

    lint issues

--- acceptance_tests/features/steps/reply_to_message_internal.py
+++ acceptance_tests/features/steps/reply_to_message_internal.py
@@ -15,21 +15,25 @@ def internal_user_receive_message(_):
     # Sending external to internal may sign out the internal user
     signed_in_internal(None)
 
+
 @when('they view the message')
 def internal_user_can_view_message(_):
     inbox_internal.go_to()
     go_to_thread()
 
-@When('they reply to the message')
+
+@when('they reply to the message')
 @then('they are able to reply')
 def internal_user_can_reply(_):
     create_message_internal.enter_text_in_message_body('Body')
     create_message_internal.click_message_send_button()
     assert 'Message sent' in create_message_internal.get_first_flashed_message()
 
+
 @then('they are to be navigated to the inbox of messages')
 def internal_user_navigated_to_inbox(_):
     assert '/messages/Bricks' in get_current_url()
+
 
 @then('they receive confirmation that the message has been sent')
 def internal_user_receives_confirmation(_):
