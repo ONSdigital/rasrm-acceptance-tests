cc9915baee9cb4be5b2234d7df8885faf8f1a07e
Emilio Ward <emilioward@hotmail.co.uk>
2018-04-13 15:30:36 +0100

    added in acceptance test scenario

---
+++ acceptance_tests/features/steps/edit_survey_details.py
@@ -0,0 +1,34 @@
+from behave import given, when, then
+
+from acceptance_tests import browser
+from acceptance_tests.features.pages import edit_survey_details_form, survey
+
+
+@given('the internal user is on the survey list page')
+def check_user_on_surveys_page(_):
+    survey.go_to()
+    assert "Surveys | Survey Data Collection" in browser.title
+
+
+@when('they request to edit/amend a surveys details')
+def user_navigates_to_edit_survey_details_page_for_nbs(_):
+    survey.click_edit_survey_details_button()
+
+
+@when('they edit/amend the survey details')
+def edit_survey_details(_):
+    edit_survey_details_form.edit_short_name('NBS 2.0')
+    edit_survey_details_form.edit_long_name('National Balance Sheet 2.0')
+    edit_survey_details_form.click_save()
+
+
+@then('they can view the updated survey details')
+def view_updated_survey_details(context):
+    surveys = survey.get_surveys()
+
+    for row in context.table:
+        survey_by_id = next(filter(lambda s: s['id'] == row['survey_id'], surveys))
+        assert survey_by_id['id'] == "199"
+        assert survey_by_id['name'] == "National Balance Sheet 2.0"
+        assert survey_by_id['short_name'] == "NBS 2.0"
+        assert survey_by_id['legal_basis'] == "Voluntary Not Stated"
