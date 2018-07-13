533dabaec5f2124de04a3a82b559d9d6669dd8ab
Ben Jefferies <benjefferies@echosoft.uk>
2018-07-13 13:28:05 +0100

    Attempt to fix error creating Chrome
    We occasionally see failures starting chrome with the error
    `Exception ConnectionResetError: [Errno 104] Connection reset by peer`.
    There seems to be an open bug around this on github https://github.com/SeleniumHQ/selenium/issues/5296
    and as a work around adding a retry mechanism to hopefully ignore it
    until it's fixed. Also updating libraries in the chance that might fix
    it.

--- acceptance_tests/__init__.py
+++ acceptance_tests/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from retrying import retry
 from selenium import webdriver
 from splinter import Browser
 from structlog import configure
@@ -9,10 +10,16 @@ from structlog.stdlib import LoggerFactory
 logging.basicConfig()
 configure(logger_factory=LoggerFactory())
 
-if os.getenv('HEADLESS', 'True') == 'True':
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--window-size=1920,1200")
-    browser = Browser('chrome', headless=True, options=chrome_options)
-else:
-    browser = Browser('chrome')
+
+@retry(retry_on_exception=lambda e: isinstance(e, ConnectionError), wait_fixed=1000, stop_max_attempt_number=30)
+def create_browser():
+    if os.getenv('HEADLESS', 'True') == 'True':
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--window-size=1920,1200")
+        return Browser('chrome', headless=True, options=chrome_options)
+    else:
+        return Browser('chrome')
+
+
+browser = create_browser()
