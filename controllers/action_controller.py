dd3a803e4cd079bec2115ffd6a73988395ce004e
Ben Jefferies <benjefferies@echosoft.uk>
2018-08-03 14:55:43 +0100

    Fix API call

--- controllers/action_controller.py
+++ controllers/action_controller.py
@@ -1,6 +1,8 @@
 import logging
+from datetime import datetime
 
 import requests
+import tzlocal
 from structlog import wrap_logger
 
 from config import Config
@@ -17,15 +19,16 @@ def create_action_rule(survey_name, period):
     action_plan_id = [action_plan['id'] for action_plan in response.json() if
                       f'{survey_name} B {period}' in action_plan['name']][0]
     logger.debug('Creating action rule', survey_name=survey_name, period=period)
-    url = f'{Config.ACTION_SERVICE}/actionrules'
+    triggerTime = datetime.now(tzlocal.get_localzone()).isoformat()
     json = {
         'actionPlanId': action_plan_id,
         'actionTypeName': 'BSNOT',
         'name': 'BSNOT+0',
         'description': 'Business Pre-notification (+0 days)',
-        'daysOffset': 0,
+        'triggerDateTime': triggerTime,
         'priority': 3
     }
+    url = f'{Config.ACTION_SERVICE}/actionrules'
     response = requests.post(url, auth=Config.BASIC_AUTH, json=json)
     response.raise_for_status()
     logger.debug('Successfully created action rule', survey_name=survey_name, period=period)
