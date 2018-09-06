1b7dfa7824e4636d989e682d66e1c37e8390747a
tej99 <tejas.patel@qa.com>
2018-09-06 13:08:08 +0100

    Removed 'GMT' and use localised time (#173)

--- acceptance_tests/features/steps/view_collection_exercise_details.py
+++ acceptance_tests/features/steps/view_collection_exercise_details.py
@@ -31,11 +31,11 @@ def internal_user_can_view_qbs_2018_collection_exercise_details(_):
 @then('the user is able to view the event dates for that collection exercise')
 def internal_user_can_view_qbs_2018_collection_exercise_events(_):
     ce_events = collection_exercise_details.get_collection_exercise_events()
-    assert ce_events['mps'] == "Wednesday 14 Mar 2018 00:00 GMT"
-    assert ce_events['go_live'] == "Monday 19 Mar 2018 00:00 GMT"
-    assert ce_events['return_by'] == "Tuesday 27 Mar 2018 00:00 GMT"
-    assert ce_events['first_reminder'] == "Tuesday 03 Apr 2018 00:00 GMT"
-    assert ce_events['exercise_end'] == "Thursday 30 Apr 2020 00:00 GMT"
+    assert ce_events['mps'] == "Wednesday 14 Mar 2018 00:00"
+    assert ce_events['go_live'] == "Monday 19 Mar 2018 00:00"
+    assert ce_events['return_by'] == "Tuesday 27 Mar 2018 01:00"
+    assert ce_events['first_reminder'] == "Tuesday 03 Apr 2018 01:00"
+    assert ce_events['exercise_end'] == "Thursday 30 Apr 2020 01:00"
 
 
 @then('the user is able to view the variable event dates for that collection exercise')
@@ -44,5 +44,5 @@ def internal_user_can_view_qbs_2018_collection_exercise_variable_event_dates(_):
     assert ce_events['ref_period_start'] == "Friday 09 Mar 2018"
     assert ce_events['ref_period_end'] == "Saturday 31 Mar 2018"
     assert ce_events['employment'] == "Friday 09 Mar 2018"
-    assert ce_events['second_reminder'] == "Monday 23 Apr 2018 00:00 GMT"
-    assert ce_events['third_reminder'] == "Saturday 05 May 2018 00:00 GMT"
+    assert ce_events['second_reminder'] == "Monday 23 Apr 2018 01:00"
+    assert ce_events['third_reminder'] == "Saturday 05 May 2018 01:00"
