d555b7ecab89b2ce5d79b8ee468e6505cfcde4b9
AdeHarrison <adrian.harrison.uk@gmail.com>
2019-01-03 11:22:05 +0000

    Converted view_reporting_unit_details.feature to standalone (#251)

--- acceptance_tests/features/steps/view_reporting_unit_details.py
+++ acceptance_tests/features/steps/view_reporting_unit_details.py
@@ -6,67 +6,59 @@ from structlog import wrap_logger
 
 from acceptance_tests import browser
 from acceptance_tests.features.pages import edit_respondent_details_form, reporting_unit
-from acceptance_tests.features.steps.authentication import signed_in_internal
-from config import Config
 from controllers.party_controller import get_party_by_ru_ref, get_respondent_details
 
 logger = wrap_logger(getLogger(__name__))
 
 
-@given('the reporting unit {ru_ref} is in the system')
-def reporting_unit_is_in_the_system(_, ru_ref):
+@given('a reporting unit is in the system')
+def reporting_unit_is_in_the_system(_):
     pass
 
 
 @given('the respondent "first_name" is enrolled')
-def respondent_first_name_is_enrolled(_):
-    business = get_party_by_ru_ref('49900000001')
+def respondent_first_name_is_enrolled(context):
+    business = get_party_by_ru_ref(context.short_name)
     respondent_id = business['associations'][0]['partyId']
     respondent = get_respondent_details(respondent_id)
     if respondent['firstName'] != 'first_name' \
             or respondent['lastName'] != 'last_name' \
-            or respondent['telephone'] != "0987654321":
-        reporting_unit.go_to('49900000001')
-        reporting_unit.click_data_panel('Bricks')
-        reporting_unit.click_edit_details('Bricks', Config.RESPONDENT_USERNAME)
+            or respondent['telephone'] != context.phone_number:
+        reporting_unit.go_to(context.short_name)
+        reporting_unit.click_data_panel(context.short_name)
+        reporting_unit.click_edit_details(context.short_name, context.respondent_email)
         edit_respondent_details_form.edit_first_name('first_name')
         edit_respondent_details_form.edit_last_name('last_name')
-        edit_respondent_details_form.edit_contact_number('0987654321')
+        edit_respondent_details_form.edit_contact_number(context.phone_number)
         edit_respondent_details_form.click_save()
 
 
-@given('the internal user is on the reporting unit page for 49900000003')
-def internal_user_is_on_reporting_unit_page(_):
-    signed_in_internal(_)
-    reporting_unit.go_to('49900000003')
+@when('the internal user views the reporting unit page')
+def internal_user_views_the_reporting_unit_page(context):
+    reporting_unit.go_to(context.short_name)
 
 
-@when('the internal user views the {ru_ref} reporting unit page')
-def internal_user_views_the_reporting_unit_page(_, ru_ref):
-    reporting_unit.go_to(ru_ref)
-
-
-@when('the internal user opens the {survey} data panel')
-def internal_user_views_the_survey_page(_, survey):
-    reporting_unit.click_data_panel(survey)
+@when('the internal user opens the data panel')
+def internal_user_views_the_survey_page(context):
+    reporting_unit.click_data_panel(context.short_name)
 
 
 @then('the internal user is displayed the correct reporting unit details')
-def internal_user_views_correct_reporting_unit_details(_):
+def internal_user_views_correct_reporting_unit_details(context):
     ru_details = reporting_unit.get_ru_details()
-    assert ru_details['ru_ref'] == '49900000001'
-    assert ru_details['name'] == 'RUNAME1_COMPANY1 RUNNAME2_COMPANY1'
+    assert ru_details['ru_ref'] == context.short_name
+    assert ru_details['name'] == 'RED BRICKS LTD'
 
 
 @then('the internal user is presented with the associated surveys')
-def internal_internal_user_presented_correct_associated_surveys(_):
+def internal_internal_user_presented_correct_associated_surveys(context):
     associated_surveys = reporting_unit.get_associated_surveys()
-    assert '074 Bricks' in associated_surveys
+    assert f'{context.survey_ref} {context.short_name}' in associated_surveys
 
 
 @then('the internal user is presented with the associated collection exercises')
-def internal_internal_user_presented_correct_associated_collection_exercises(_):
-    associated_ces = reporting_unit.get_associated_collection_exercises('Bricks')
+def internal_internal_user_presented_correct_associated_collection_exercises(context):
+    associated_ces = reporting_unit.get_associated_collection_exercises(context.short_name)
     # Status updated async so wait until updated
     for i in range(5):
         if 'Not started' in associated_ces[0]['status']:
@@ -74,37 +66,31 @@ def internal_internal_user_presented_correct_associated_collection_exercises(_):
         browser.reload()
         associated_ces = reporting_unit.get_associated_collection_exercises('Bricks')
         time.sleep(1)
-    exercise_201801 = reporting_unit.get_collection_exercise('201801', 'Bricks')
-    assert exercise_201801['exercise_ref'] == '201801'
-    assert exercise_201801['company_name'] == 'RUNAME1_COMPANY1 RUNNAME2_COMPANY1'
-    assert exercise_201801['company_region'] == 'GB'
-    assert 'Not started' in exercise_201801['status'], exercise_201801['status']
+    exercise = reporting_unit.get_collection_exercise(context.period, context.short_name)
+    assert exercise['exercise_ref'] == context.period
+    assert exercise['company_name'] == 'RED BRICKS LTD'
+    assert exercise['company_region'] == 'GB'
+    assert 'Not started' in exercise['status'], exercise['status']
 
 
 @then('the internal user is presented with the associated respondents')
-def internal_internal_user_presented_correct_associated_respondents(_):
+def internal_internal_user_presented_correct_associated_respondents(context):
     # Status updated async so wait until updated
-    respondent = reporting_unit.get_respondent('Bricks', 'example@example.com')
+    respondent = reporting_unit.get_respondent(context.short_name, context.respondent_email)
     assert respondent['enrolmentStatus'] == 'Enabled'
     assert respondent['name'] == 'first_name last_name'
-    assert respondent['email'] == 'example@example.com'
-    assert respondent['phone'] == '0987654321'
+    assert respondent['email'] == context.respondent_email
+    assert respondent['phone'] == context.phone_number
     assert respondent['accountStatus'] == 'Active'
 
 
-@then('the status \'Completed by phone\' is displayed back to the internal user')
-def status_is_displayed_back_the_internal_user(_):
-    associated_ces = reporting_unit.get_associated_collection_exercises('Bricks')
-    assert 'Completed by phone' in associated_ces[0]['status']
-
-
 @then('the "trading as" name should be displayed on the RU details')
-def trading_as_name_is_displayed(_):
-    assert 'PC LTD' in browser.driver.find_element_by_id('RU_DETAILS').text,\
-        'Could not find trading as name "PC LTD" in respondent details page'
+def trading_as_name_is_displayed(context):
+    assert context.short_name in browser.driver.find_element_by_id('RU_DETAILS').text, \
+        f'Could not find trading as name "{context.short_name}" in respondent details page'
 
 
 @then('the "trading as" name should not appear on the RU details')
-def trading_as_name_does_not_appear(_):
+def trading_as_name_does_not_appear(context):
     assert 'Trading as:' not in browser.driver.find_element_by_id('RU_DETAILS').text,\
         '"Trading as" is displayed when the business has no trading name'
--- acceptance_tests/features/view_reporting_unit_details.feature
+++ acceptance_tests/features/view_reporting_unit_details.feature
@@ -1,3 +1,5 @@
+@business
+@standalone
 Feature: View reporting unit details
   As an internal user
   I need to be able to view the surveys, CEs and respondents associated to the RU Ref
@@ -8,41 +10,47 @@ Feature: View reporting unit details
     Given the internal user is already signed in
 
   @us044_s01
+  @fixture.setup.data.with.internal.user.and.collection.exercise.to.ready.for.live.status
   Scenario: The reporting unit details are presented to the user
-    Given the reporting unit 49900000001 is in the system
-    When the internal user views the 49900000001 reporting unit page
+    Given a reporting unit is in the system
+    When the internal user views the reporting unit page
     Then the internal user is displayed the correct reporting unit details
 
   @us045_s01
+  @fixture.setup.data.with.internal.user.and.collection.exercise.to.ready.for.live.status
   Scenario: Able to view all surveys associated to the displayed RU Ref
-    Given the reporting unit 49900000001 is in the system
-    When the internal user views the 49900000001 reporting unit page
+    Given a reporting unit is in the system
+    When the internal user views the reporting unit page
     Then the internal user is presented with the associated surveys
 
   @us045_s02
+  @fixture.setup.data.with.internal.user.and.collection.exercise.to.ready.for.live.status
   Scenario: Able to view all collection exercises associated to the displayed RU Ref
-    Given the reporting unit 49900000001 is in the system
-    When the internal user views the 49900000001 reporting unit page
-    And the internal user opens the Bricks data panel
+    Given a reporting unit is in the system
+    When the internal user views the reporting unit page
+    And the internal user opens the data panel
     Then the internal user is presented with the associated collection exercises
 
   @us046_s01
+  @fixture.setup.data.with.enrolled.respondent.user.and.internal.user
   Scenario: Able to view respondent details for the RU Ref for the survey
-    Given the reporting unit 49900000001 is in the system
+    Given a reporting unit is in the system
     And the respondent "first_name" is enrolled
-    When the internal user views the 49900000001 reporting unit page
-    And the internal user opens the Bricks data panel
+    When the internal user views the reporting unit page
+    And the internal user opens the data panel
     Then the internal user is presented with the associated respondents
 
   @us047_s01
-  Scenario: "Trading as" name is displayed on Reporting Units page
-    Given the reporting unit 49900000007 is in the system
-    When the internal user views the 49900000007 reporting unit page
+  @fixture.setup.data.with.internal.user.and.collection.exercise.to.ready.for.live.status
+  Scenario: Trading as name is displayed on Reporting Units page
+    Given a reporting unit is in the system
+    When the internal user views the reporting unit page
     Then the "trading as" name should be displayed on the RU details
 
   @us047_s01
-  Scenario: If no "Trading As" name, no further information should be provided
-    Given the reporting unit 49900000008 is in the system
-    When the internal user views the 49900000008 reporting unit page
+  @fixture.setup.data.with.internal.user.and.collection.exercise.to.ready.for.live.status
+  Scenario: If no Trading As name, no further information should be provided
+    Given a reporting unit is in the system
+    When the internal user views the reporting unit page
     Then the "trading as" name should not appear on the RU details
 
