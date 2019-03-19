373528cbde570c838f3829722d2bbeaabf77d75f
Owen Jones <owencjones+github@gmail.com>
2019-03-19 16:18:16 +0000

    Removed commented out function (#268)

--- acceptance_tests/features/steps/find_respondent_by_email.py
+++ acceptance_tests/features/steps/find_respondent_by_email.py
@@ -11,11 +11,6 @@ def create_respondent_account(context):
     assert respondent_party is not None, f'No respondent with email {context.respondent_email} exists'
 
 
-
-# def search_respondent_by_email(context):
-#     respondent.go_to_find_respondent()
-#     respondent.search_respondent_by_email(context.respondent_email)
-
 @when('an internal user searches for respondent using their email address')
 @given('the internal user has navigated to a respondents details page')
 def go_to_respondent_detail_page(context):
